047eb2a6b354966530c60b44c15818766b426e5c
benjaminpants <maltbyfamily@gmail.com>
2023-11-20 01:40:06 -0800

    wait for seconds timescale for coroutines

---
+++ MTM101BMDE/Registers/ItemManagement.cs
@@ -0,0 +1,23 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MTM101BaldAPI.Registers
+{
+    // flags for items so mods know how to handle them
+    public enum ItemFlags
+    {
+        MultipleUse = 1, // This item has multiple uses like the grappling hook.
+        NoInventory = 2, // This item should not appear in the players inventory. This is useful for stuff like presents or non-items that use the Pickup system.
+        NoUses = 4, // This item doesn't do anything when used, regardless of circumstance. This is for items like the Apple, but not the quarter as it can be used in machines.
+    }
+
+    public struct ItemMetaData
+    {
+        public ItemObject itemObject;
+        public ItemFlags flags;
+        public Items id;
+    }
+}
+*/
---
+++ MTM101BMDE/WaitForSecondsTimescales.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace MTM101BaldAPI
+{
+    public class WaitForSecondsNPCTimescale : CustomYieldInstruction
+    {
+        public EnvironmentController ec;
+        public float timeRemaining;
+
+        public WaitForSecondsNPCTimescale(EnvironmentController envC, float seconds)
+        {
+            timeRemaining = seconds;
+            ec = envC;
+        }
+
+        public override bool keepWaiting
+        {
+            get
+            {
+                if (!ec) return false; // EC GONE! Establish CHAOS!
+                timeRemaining -= Time.deltaTime * ec.NpcTimeScale;
+                return (timeRemaining >= 0);
+            }
+        }
+    }
+
+    public class WaitForSecondsEnviromentTimescale : CustomYieldInstruction
+    {
+        public EnvironmentController ec;
+        public float timeRemaining;
+
+        public WaitForSecondsEnviromentTimescale(EnvironmentController envC, float seconds)
+        {
+            timeRemaining = seconds;
+            ec = envC;
+        }
+
+        public override bool keepWaiting
+        {
+            get
+            {
+                if (!ec) return false; // EC GONE! Establish CHAOS!
+                timeRemaining -= Time.deltaTime * ec.EnvironmentTimeScale;
+                return (timeRemaining >= 0);
+            }
+        }
+    }
+}
