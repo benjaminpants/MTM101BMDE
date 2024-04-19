6147becce9a5c7d90346af09d7e4ea3f9b48ce34
Sakyceh <55030860+Sakyce@users.noreply.github.com>
2024-04-19 20:45:22 +0200

    Added XML docs to enums

--- MTM101BMDE/Registers/GeneratorManagement.cs
+++ MTM101BMDE/Registers/GeneratorManagement.cs
@@ -8,9 +8,25 @@ namespace MTM101BaldAPI.Registers
 {
     public enum GenerationModType
     {
+        /// <summary>
+        /// This should be used for methods that override the majority of the generator properties, almost completely transforming the level.
+        /// </summary>
         Base, // runs first, expected to override everything
+
+        /// <summary>
+        /// This should be used for overriding only certain properties of the generator, such as changing exit counts.
+        /// </summary>
         Override, // overrides specific variables, such as items and whatnot
+
+        /// <summary>
+        /// This should be used for adding onto already existing properties, such as adding characters or items. Most mods will be using this.
+        /// </summary>
         Addend, // adds to already existing fields, such as adding new items
+
+        /// <summary>
+        /// Useful for removing things that might've been added or changed by other mods, or if for one reason or another you need the final say on something. 
+        /// <c>Use with caution.</c>
+        /// </summary>
         Finalizer // runs last, useful for subtracting/removing things or if you REALLY need the last say, use with caution!
     }
 
