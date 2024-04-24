d13290cb51e29ac906d2994cf8e210ee962f20b3
benjaminpants <maltbyfamily@gmail.com>
2024-04-23 19:35:49 -0700

    add PlayerMovementStatModifier(not implemented yet)

---
+++ MTM101BMDE/Components/ValueModifier.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace MTM101BaldAPI.Components
+{
+
+    public static class ValueModifierExtensions
+    {
+        public static float Multiplier(this List<ValueModifier> modifiers)
+        {
+            float num = 1f;
+            foreach (ValueModifier vm in modifiers)
+            {
+                num *= vm.multiplier;
+            }
+            return num;
+        }
+
+        public static float Addend(this List<ValueModifier> modifiers)
+        {
+            float num = 0f;
+            for (int i = 0; i < modifiers.Count; i++)
+            {
+                num += modifiers[i].addend;
+            }
+            return num;
+        }
+    }
+
+    public abstract class ValueModifierManager<T> : MonoBehaviour
+    {
+
+        void Awake()
+        {
+            Initialize();
+        }
+
+        public abstract void Initialize();
+
+        public abstract ValueModifier[] Modifiers { get; }
+
+        public abstract void RemoveModifier(ValueModifier vm);
+
+        public abstract void AbstractUpdate();
+
+        void Update()
+        {
+            AbstractUpdate();
+        }
+    }
+
+    public class ValueModifier
+    {
+
+        public ValueModifier(float multiplier = 1f, float addend = 0f)
+        {
+            this.multiplier = multiplier;
+            this.addend = addend;
+        }
+
+        public float multiplier;
+        public float addend;
+    }
+}
---
+++ MTM101BMDE/PlusExtensions/PlayerMovementStatModifier.cs
@@ -0,0 +1,61 @@
+﻿using HarmonyLib;
+using MTM101BaldAPI.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MTM101BaldAPI.PlusExtensions
+{
+    public class PlayerMovementStatModifier : ValueModifierManager<PlayerMovement>
+    {
+        PlayerMovement pm;
+
+        Dictionary<string, List<ValueModifier>> modifiers = new Dictionary<string, List<ValueModifier>>();
+
+        Dictionary<string, float> baseStats = new Dictionary<string, float>();
+        public override ValueModifier[] Modifiers => throw new NotImplementedException();
+
+        float GetModifiedStat(string name)
+        {
+            return (baseStats[name] * (modifiers[name].Multiplier())) + (modifiers[name].Addend());
+        }
+
+        void AddBaseStat(string name, float value)
+        {
+            baseStats.Add(name, value);
+            modifiers.Add(name, new List<ValueModifier>());
+        }
+
+        public override void AbstractUpdate()
+        {
+            pm.staminaRise = GetModifiedStat("staminaRise");
+            pm.staminaMax = GetModifiedStat("staminaMax");
+            pm.staminaDrop = GetModifiedStat("staminaDrop");
+            pm.walkSpeed = GetModifiedStat("walkSpeed");
+            pm.runSpeed = GetModifiedStat("runSpeed");
+        }
+
+        public void ChangeBaseStat(string statToChange, float value)
+        {
+            baseStats[statToChange] = value;
+        }
+
+        public override void Initialize()
+        {
+            pm = GetComponent<PlayerMovement>();
+            AddBaseStat("staminaRise", pm.staminaRise);
+            AddBaseStat("staminaMax", pm.staminaMax);
+            AddBaseStat("staminaDrop", pm.staminaDrop);
+            AddBaseStat("walkSpeed", pm.walkSpeed);
+            AddBaseStat("runSpeed", pm.runSpeed);
+        }
+
+        public override void RemoveModifier(ValueModifier vm)
+        {
+            modifiers.Do(x =>
+            {
+                x.Value.Remove(vm);
+            });
+        }
+    }
+}
