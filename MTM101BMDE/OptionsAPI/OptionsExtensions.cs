4c4012867418cba2c2fcefd31703dffc069248cb
benjaminpants <maltbyfamily@gmail.com>
2024-11-23 18:20:55 -0800

    Create OptionsExtensions.cs

---
+++ MTM101BMDE/OptionsAPI/OptionsExtensions.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace MTM101BaldAPI.OptionsAPI
+{
+    public static class OptionsExtensions
+    {
+        static FieldInfo _val = AccessTools.Field(typeof(AdjustmentBars), "val");
+
+        public static int GetRaw(this AdjustmentBars b)
+        {
+            return (int)_val.GetValue(b);
+        }
+    }
+}
