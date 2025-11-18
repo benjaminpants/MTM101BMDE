4104b4cd4fa069a0559c4ae89114dcd145262eab
AlexBW145 <92541723+AlexBW145@users.noreply.github.com>
2025-11-17 22:07:15 -0600

    Create DummyPlayerFixers.cs

---
+++ MTM101BMDE/Components/DummyPlayerFixers.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace MTM101BaldAPI.Components
+{
+    internal class DummyPlayerFixers : MonoBehaviour
+    {
+        private ItemManager itm;
+        private void Awake()
+        {
+            itm = GetComponent<ItemManager>();
+        }
+        private void OnDestroy()
+        {
+            if (StickerManager.Instance != null)
+                StickerManager.Instance.OnStickerApplied -= itm.UpdateTargetInventorySize;
+        }
+    }
+
+}
