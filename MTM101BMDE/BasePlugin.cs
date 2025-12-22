627c1a861ec6673f4a1450466f3b9a57696f2e8a
benjaminpants <maltbyfamily@gmail.com>
2025-12-22 15:15:52 -0800

    Update BasePlugin.cs

--- MTM101BMDE/BasePlugin.cs
+++ MTM101BMDE/BasePlugin.cs
@@ -58,7 +58,7 @@ namespace MTM101BaldAPI
     {
         internal static ManualLogSource Log = new ManualLogSource("Baldi's Basics Plus Dev API Pre Initialization");
         public const string ModGUID = "mtm101.rulerp.bbplus.baldidevapi";
-        public const string VersionNumber = "10.1.0.0";
+        public const string VersionNumber = "10.2.0.0";
 
         /// <summary>
         /// The version of the API, applicable when BepInEx cache messes up the version number.
