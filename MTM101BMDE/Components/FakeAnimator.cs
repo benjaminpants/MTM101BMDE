3f9bb54bf0dd4ff84aad4e0aa1a9b7e929b74cf8
benjaminpants <maltbyfamily@gmail.com>
2026-09-18 14:24:10 -0700

    Update FakeAnimator.cs

--- MTM101BMDE/Components/FakeAnimator.cs
+++ MTM101BMDE/Components/FakeAnimator.cs
@@ -4,7 +4,6 @@ using MTM101BaldAPI.Components.Animation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
